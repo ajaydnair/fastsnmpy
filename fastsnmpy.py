90b30d6007b412fcfdee00a659701816b3ff865f
ajaysdesk <ajaysdesk@gmail.com>
2016-01-24 11:20:41 -0500

    Do we really need 4 methods? Since two of these methods are just revisions of the earlier methods with a larger number of workers..
    So i've shortened it to just 2 methods.
    
    >> snmpwalk(workers = 1): snmpwalk using methods from net-snmp bindings.
    Increase workers (optional) to call upon a pool of processes for faster runs.
    
    >> snmpbulkwalk(workers = 1): newest implementation. Much faster than
    methods from python-bindings for net-snmp. Improvises on the getbulk method.
    
    Also de-cluttered and modified code to use the stable multiprocessing library.
    
    Python has never been great with threads. Some benchmarks
    can be seen here : http://ajaydivakaran.com/multithreading-vs-multiprocessing/

--- fastsnmpy.py
+++ fastsnmpy.py
@@ -1,183 +1,246 @@
 
-""" fastsnmpy is a collection of classes that greatly increase
+''' 
+
+fastsnmpy is a collection of classes that greatly increase
 the speed of snmp discovery, providing methods like snmpbulkwalk
-and multiwalk (for multithreaded snmp) """
+and multiwalk (for multiprocess snmp). 
+
+snmpbulkwalk is not a feature 
+of the python-bindings that come with net-snmp as of this release.
+By leveraging the getbulk method, this module provides a quick 
+snmpbulkwalk utility.
+
+    name      = 'fastsnmpy',
+    version     = '1.2.1',
+    py_modules   = ['fastsnmpy','example'],
+    author     = 'Ajay Divakaran',
+    author_email  = 'ajaysdesk@gmail.com'
+    url       = 'http://www.ajaydivakaran.com/fastsnmpy',
+    description   = 'Superfast snmp bulkwalk method 
+        implemented using net-snmp bindings for getbulk'
+
+PROVIDES METHODS:
+    snmpwalk(workers=1): runs snmpwalk using net-snmp's bindings
+    snmpbulkwalk(workers=1): runs the faster snmpbulkwalk implemented here
+ '''
+
+# -------
+# IMPORTS
+# -------
+
 import netsnmp
 from multiprocessing import Process, Queue
 
+
+# -------
+# CLASSES
+# -------
+
 class SnmpSession:
-    """ The main snmpsession class gets defined here. You may 
-    later call one of the several methods defined on this class"""
+
+    ''' The main snmpsession class gets defined here. You may 
+    later call one of the several methods defined on this class'''
+
     def __init__(self,
-                oidlist = ["sysDescr"],
-                version = 2,
-                targets = [""],
-                community = "public",
-                maxreps  = 8,
-                ):
-        self.oidlist = oidlist        
+        oidlist = ['sysDescr'],
+        version = 2,
+        targets = [''],
+        community = 'public',
+        maxreps = 8,
+    ):
+
+        self.oidlist = oidlist    
         self.version = version
         self.targets = targets 
         self.community = community
         self.maxreps = maxreps
         self.results = []
 
-    def seqwalk(self):
-        """ This is the most basic walk. It runs the default 
-        snmpwalk from netsnmp's python bindings """
+
+    def snmpwalk(self):
+
+        ''' This is the most basic walk. It runs the default 
+        snmpwalk from netsnmp's python bindings '''
+
         for target in self.targets:
             for oid in self.oidlist:
-                print "Getting %s from %s " %(oid,target)
-                # Instantiate Session class from netsnmp
+
+                print 'Getting %s from %s ' %(oid,target)
+
+                # -- query each node-mib combo, and pop into results
                 mysession = netsnmp.Session(
-                                    Version = self.version,
-                                    DestHost = target,
-                                    Community = self.community
-                                    )
-                vars = netsnmp.VarList(
-                        netsnmp.Varbind(oid)
-                        )
+                    Version = self.version,
+                    DestHost = target,
+                    Community = self.community
+                )
+                
+                vars = netsnmp.VarList( netsnmp.Varbind(oid))
                 result = mysession.walk(vars)
-                # Format and returns result
+
+                # -- Format and returns result
                 for i in vars:
                     i.hostname = target
                     i.oid = oid
                     self.results.append(i)
+
         return self.results
-        
-    def bulkwalk(self):
-        """ This class uses the default getbulk methods to
-        actually provide a bulkwalk method superior to snmpwalk"""
+
+
+    def snmpbulkwalk(self):
+
+        ''' Bulkwalk is essentially a series of getBulk operations, just
+        like walk is a series of get(getNext) operations.
+
+        This is where the power and speed of fastsnmpy shows, as this method
+        is missing from native net-snmp python bindings '''
+
         for target in self.targets:
             for oid in self.oidlist:
-                print "Getting %s from %s " %(oid,target)
-                # Instantiate Session class from netsnmp
+
+                print 'Getting %s from %s ' %(oid,target)
+
+                # -- query each node-mib combo, and pop into results
                 mysession = netsnmp.Session(
-                                    Version = self.version,
-                                    DestHost = target,
-                                    Community = self.community
-                                    )
+                    Version = self.version,
+                    DestHost = target,
+                    Community = self.community
+                )
+                
                 # Start from ifindex 0, and use getbulk operations
                 startindex = 0
+
                 thistree = oid
                 while (thistree == oid):
-                    vars = netsnmp.VarList(
-                            netsnmp.Varbind(oid,startindex)
-                            )
+
+                    vars = netsnmp.VarList(netsnmp.Varbind(oid,startindex))
                     result = mysession.getbulk(0,self.maxreps,vars)
+
                     for i in vars:
                         if i.tag == thistree:
                             i.hostname = target
                             i.oid = oid
                             self.results.append(i)
+
                     # If startindex is null ( Bug Fix )
-                    if not vars[-1].iid:
-                        break
+                    if not vars[-1].iid: break
+
                     # Refresh thistree name and increment startindex
                     else:
-                        thistree = vars[-1].tag
-                        startindex = int(vars[-1].iid)
+                      thistree = vars[-1].tag
+                      startindex = int(vars[-1].iid)
+
                     # If startindex still 0 ( Bug Fix for 6500s )
-                    if startindex == 0 :
-                        break
-        return self.results
-        
-    def multiwalk(self,mode = 'seqwalk'):
-        """ Multiwalk is a multithreaded implementation. It uses
-        either sequential or getbulk methods, to walk a node """
+                    if startindex == 0 : break
 
-        ### Define number of threads
-        hosts = self.targets
-        numthreads = len(hosts)
-
-        for thisoid in self.oidlist:
-            #Create queues for threadinput and threadoutput
-            threadinput = Queue()
-            threadoutput = Queue()
-            #put each targethost into the queue
-            for target in self.targets:
-                threadinput.put(target)
-            #Call the thread worker process on the queue for as many targets
-            for i in range(numthreads):
-                Process(target=self.multiworker, 
-                        args=(threadinput, threadoutput,thisoid,mode)).start()
-            #Collect results from threads
-            for i in range(numthreads):
-                myresp = threadoutput.get()
-                for resp in myresp:
-                    self.results.append(resp)
-            #Tell child processes to stop
-            for i in range(numthreads):
-                threadinput.put('STOP')
-                print "Stopping %s" %i
-
-        return self.results    
-            
-    # Function run by worker processes
-    def multiworker(caller,input, output, oid, mode):
-        for target in iter(input.get, 'STOP'):
-            print "Getting %s from %s " %(oid,target)
-            threadresults = []
-            mysession = netsnmp.Session(
-                                Version = caller.version,
-                                DestHost = target,
-                                Community = caller.community
-                                        )
-            if mode == 'seqwalk':
-                # For seqwalk mode, we just call netsnmp's implementn
-                vars = netsnmp.VarList(
-                        netsnmp.Varbind(oid)
-                        )
-                result = mysession.walk(vars)
-                for i in vars:
-                    i.hostname = target
-                    i.oid = oid
-                    threadresults.append(i)
-
-            if mode == 'bulkwalk':
-                # Our custom method to do bulkwalks, very speedy!
-                startindex = 0
-                thistree = oid
-                while (thistree == oid):
-                    vars = netsnmp.VarList(
-                            netsnmp.Varbind(oid,startindex)
-                            )
-                    result = mysession.getbulk(0,caller.maxreps,vars)
+                    # -- Format and returns result
                     for i in vars:
-                        if i.tag == thistree:
-                            i.hostname = target
-                            i.oid = oid
-                            threadresults.append(i)
-                    # If startindex is null ( Bug Fix )
-                    if not vars[-1].iid:
-                        break
-                    # Refresh thistree name and increment startindex
-                    else:
-                        thistree = vars[-1].tag
-                        startindex = int(vars[-1].iid)
-                    # If startindex still 0 ( Bug Fix for 6500s )
-                    if startindex == 0 :
-                        break
+                      i.hostname = target
+                      i.oid = oid
+                      self.results.append(i)
 
-            output.put(threadresults)
+        return self.results
 
-""" Actual code begins below this line. The classes above dont need
+
+
+
+'''    
+    
+  def multiwalk(self,mode = 'snmpwalk'):
+
+    ### Define number of threads
+    hosts = self.targets
+    numthreads = len(hosts)
+
+    for thisoid in self.oidlist:
+      #Create queues for threadinput and threadoutput
+      threadinput = Queue()
+      threadoutput = Queue()
+      #put each targethost into the queue
+      for target in self.targets:
+        threadinput.put(target)
+      #Call the thread worker process on the queue for as many targets
+      for i in range(numthreads):
+        Process(target=self.multiworker, 
+            args=(threadinput, threadoutput,thisoid,mode)).start()
+      #Collect results from threads
+      for i in range(numthreads):
+        myresp = threadoutput.get()
+        for resp in myresp:
+          self.results.append(resp)
+      #Tell child processes to stop
+      for i in range(numthreads):
+        threadinput.put('STOP')
+        print 'Stopping %s' %i
+
+    return self.results  
+      
+  # Function run by worker processes
+  def multiworker(caller,input, output, oid, mode):
+    for target in iter(input.get, 'STOP'):
+      print 'Getting %s from %s ' %(oid,target)
+      threadresults = []
+      mysession = netsnmp.Session(
+                Version = caller.version,
+                DestHost = target,
+                Community = caller.community
+                    )
+      if mode == 'snmpwalk':
+        # For snmpwalk mode, we just call netsnmp's implementn
+        vars = netsnmp.VarList(
+            netsnmp.Varbind(oid)
+            )
+        result = mysession.walk(vars)
+        for i in vars:
+          i.hostname = target
+          i.oid = oid
+          threadresults.append(i)
+
+      if mode == 'bulkwalk':
+        # Our custom method to do bulkwalks, very speedy!
+        startindex = 0
+        thistree = oid
+        while (thistree == oid):
+          vars = netsnmp.VarList(
+              netsnmp.Varbind(oid,startindex)
+              )
+          result = mysession.getbulk(0,caller.maxreps,vars)
+          for i in vars:
+            if i.tag == thistree:
+              i.hostname = target
+              i.oid = oid
+              threadresults.append(i)
+          # If startindex is null ( Bug Fix )
+          if not vars[-1].iid:
+            break
+          # Refresh thistree name and increment startindex
+          else:
+            thistree = vars[-1].tag
+            startindex = int(vars[-1].iid)
+          # If startindex still 0 ( Bug Fix for 6500s )
+          if startindex == 0 :
+            break
+
+      output.put(threadresults)
+'''
+
+''' Actual code begins below this line. The classes above dont need
 to be modified. They can just be copied and pasted .
 Installation is a much cleaner way though. 
-You can see the documentation at http://www.ajaydivakaran.com/fastsnmpy"""
+You can see the documentation at http://www.ajaydivakaran.com/fastsnmpy'''
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     #clients and Oids
-    hosts =['c7200-2']
+    hosts =['c7200-2','c7200-1','c2600-1','c2600-2']
     oids = ['ifDescr']
 
     newsession = SnmpSession ( targets = hosts, 
-            oidlist = oids,
-            community='oznet' 
-            )
-#    results = newsession.bulkwalk()   # For seqwalk -default
-    results = newsession.multiwalk(mode = 'bulkwalk')
+        oidlist = oids,
+        community='oznet' 
+    )
+
+    results = newsession.snmpbulkwalk()  # For snmpwalk -default
+    #  results = newsession.multiwalk(mode = 'bulkwalk')
     for vb in results:
         print vb.__dict__
 
